f4791128f9b12a8ec62ddac55ca82bc7339d9baf
kalimisettyraviteja <kalimisettyraviteja@gmail.com>
2026-06-24 01:15:37 +0530

    fix: cross url support local + vercel for render ping

--- user-service/src/main/java/com/fooddelivery/userservice/controller/PingController.java
+++ user-service/src/main/java/com/fooddelivery/userservice/controller/PingController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://render-ping.vercel.app/home")
-public class PingController {
+@CrossOrigin(origins = {
+        "http://localhost:4200",
+        "https://render-ping.vercel.app"
+})public class PingController {
 
     @GetMapping("/ping")
     public String ping() {
