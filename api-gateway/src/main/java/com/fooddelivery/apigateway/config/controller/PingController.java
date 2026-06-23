a34fae8d1d24430a6a2f116c93d838a5eac961b8
kalimisettyraviteja <kalimisettyraviteja@gmail.com>
2026-06-24 01:22:29 +0530

    fix: cross url for render ping

--- api-gateway/src/main/java/com/fooddelivery/apigateway/config/controller/PingController.java
+++ api-gateway/src/main/java/com/fooddelivery/apigateway/config/controller/PingController.java
@@ -4,10 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {
-        "http://localhost:4200",
-        "https://render-ping.vercel.app"
-})public class PingController {
+@CrossOrigin(origins = "https://render-ping.vercel.app")
+public class PingController {
 
     @GetMapping("/ping")
     public String ping() {
