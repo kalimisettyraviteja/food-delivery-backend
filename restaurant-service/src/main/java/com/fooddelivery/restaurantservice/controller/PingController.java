92809303f14ed19d3302bbf246c0496f4e60b061
kalimisettyraviteja <kalimisettyraviteja@gmail.com>
2026-06-23 23:42:43 +0530

    fix: cross fix to support localhost

--- restaurant-service/src/main/java/com/fooddelivery/restaurantservice/controller/PingController.java
+++ restaurant-service/src/main/java/com/fooddelivery/restaurantservice/controller/PingController.java
@@ -1,9 +1,11 @@
 package com.fooddelivery.restaurantservice.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PingController {
 
     @GetMapping("/ping")
